b557fb437c78a481bb0302b9dab6000dd222075b
Aline Pinho <alineveira2@gmail.com>
2025-05-09 12:01:26 -0300

    Create Usuario.java

---
+++ sistema-cadastro-pessoas/src/java/br/com/etb/model/Usuario.java
@@ -0,0 +1,74 @@
+package br.com.etb.model;
+
+/**
+ *
+ * @author Aline
+ */
+public class Usuario {
+    private int id;
+    private String nome;
+    private String email;
+    private String senha;
+    private int nivelAcesso;
+
+    // Construtor
+    public Usuario() {
+    }
+    
+    public Usuario(int id, String nome, String email, String senha,
+            int nivelAcesso) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.nivelAcesso = nivelAcesso;
+    }
+    
+    // Método para exibir os dados do usuário
+    @Override
+    public String toString() {
+        return "Usuario [id=" + id + ", nome=" + nome + ", email=" + email + ", nivelAcesso=" + nivelAcesso + "]";
+    }
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getNivelAcesso() {
+        return nivelAcesso;
+    }
+
+    public void setNivelAcesso(int nivelAcesso) {
+        this.nivelAcesso = nivelAcesso;
+    }
+}
+
